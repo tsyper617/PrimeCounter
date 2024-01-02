92d514fed8a0eed25fc149f986dafbe41400384b
tsyper617 <155414005+tsyper617@users.noreply.github.com>
2024-01-01 23:51:35 -0500

    Add files via upload

---
+++ PrimeCounter.java
@@ -0,0 +1,38 @@
+import stdlib.StdOut;
+
+public class PrimeCounter {
+    // Entry point. [DO NOT EDIT]
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        StdOut.println(primes(n));
+    }
+
+    // Returns true if x is prime; and false otherwise.
+    private static boolean isPrime(int x) {
+        // For each 2 <= i <= x / i, if x is divisible by i, then x is not a prime. If no such i
+        // exists, then x is a prime.
+        if (x > 2 && x % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i < Math.sqrt(x) + 1; i += 2) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+
+    // Returns the number of primes <= n.
+    private static int primes(int n) {
+        // For each 2 <= i <= n, use isPrime() to test if i is prime, and if so increment a count.
+        // At the end return the count.
+        int primes = 0;
+        for (int i = 2; i <= n; i++) {
+            primes += isPrime(i) ? 1 : 0;
+        }
+        return primes;
+    }
+}
